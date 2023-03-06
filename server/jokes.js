dee51e0cea1adc21b8a7de439e59f2ab40160720
baodevman <bao.devman@gmail.com>
2023-03-06 11:17:20 +0700

    remove p tag from text

--- server/jokes.js
+++ server/jokes.js
@@ -1,7 +1,7 @@
 const jokes = {
   "joke1": {
     "id": "JK01",
-    "content": "<p>A child asked his father, \"How were people born?\" So his father said, \"Adam and Eve made babies, then their babies became adults and made babies, and so on.\"</p><p>The child then went to his mother, asked her the same question and she told him, \"We were monkeys then we evolved to become like we are now.\"</p><p>The child ran back to his father and said, \"You lied to me!\" His father replied, \"No, your mom was talking about her side of the family.\"</p>"
+    "content": "A child asked his father, \"How were people born?\" So his father said, \"Adam and Eve made babies, then their babies became adults and made babies, and so on.\"The child then went to his mother, asked her the same question and she told him, \"We were monkeys then we evolved to become like we are now.\"The child ran back to his father and said, \"You lied to me!\" His father replied, \"No, your mom was talking about her side of the family.\""
   },
   "joke2": {
     "id": "JK02",
